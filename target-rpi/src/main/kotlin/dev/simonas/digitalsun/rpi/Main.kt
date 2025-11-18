be9b738985252abc7fd521fb027d48292e458eb8
Simonas Sankauskas <simonas.sank@gmail.com>
2025-11-18 13:47:53 +0200

    move to spi pin

--- target-rpi/src/main/kotlin/dev/simonas/digitalsun/rpi/Main.kt
+++ target-rpi/src/main/kotlin/dev/simonas/digitalsun/rpi/Main.kt
@@ -23,7 +23,7 @@ fun start() {
     val stage = Stage()
     val pixels = stage.getPixels()
     val ledCount = pixels.size
-    val gpioPin = 18
+    val gpioPin = 10
 
     println("Initializing $ledCount LEDs on GPIO $gpioPin...")
 
