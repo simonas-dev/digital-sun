e4e9898e0eb4374c1eaea5c827c069a3e05a3c36
Simonas Sankauskas <simonas.sank@gmail.com>
2025-11-18 13:53:46 +0200

    fix vibes in deploy script

--- deploy-rpi.sh
+++ deploy-rpi.sh
@@ -6,7 +6,7 @@ set -e
 # Configuration
 RPI_HOST="${RPI_HOST:-raspberrypi.local}"
 RPI_USER="${RPI_USER:-simonas}"
-RPI_DIR="${RPI_DIR:-~/digital-sun}"
+RPI_DIR="${RPI_DIR:-~/digital-sun/target-rpi}"
 
 echo "============================================"
 echo "Digital Sun - Raspberry Pi Deployment"
@@ -28,15 +28,9 @@ fi
 echo "Build successful!"
 echo ""
 
-# Create remote directory if it doesn't exist
-echo "Preparing remote directory..."
-ssh "$RPI_USER@$RPI_HOST" "mkdir -p $RPI_DIR"
-
 # Deploy the JAR and run script
 echo "Deploying JAR and run script to Raspberry Pi..."
 scp target-rpi/build/libs/target-rpi-1.0.0.jar "$RPI_USER@$RPI_HOST:$RPI_DIR/"
-scp target-rpi/run.sh "$RPI_USER@$RPI_HOST:$RPI_DIR/"
-ssh "$RPI_USER@$RPI_HOST" "chmod +x $RPI_DIR/run.sh"
 
 echo "Running on remote..."
 echo "Press Ctrl+C to stop"
