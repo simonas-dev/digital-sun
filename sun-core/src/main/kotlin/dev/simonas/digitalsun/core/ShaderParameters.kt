f0b625682fe14427b7367f896b67f8ba1978f0d9
Simonas Sankauskas <simonas.sank@gmail.com>
2025-11-18 13:48:00 +0200

    increase redness in shader

--- sun-core/src/main/kotlin/dev/simonas/digitalsun/core/ShaderParameters.kt
+++ sun-core/src/main/kotlin/dev/simonas/digitalsun/core/ShaderParameters.kt
@@ -21,9 +21,10 @@ data class HueRange(
          * Warm color range: magenta (300°) through red (0°) to yellow (60°)
          */
 //        val WARM = HueRange(min = 0.833333, max = 0.166667)
-//       val FAINAS_V1 = HueRange(min = 355.0 / 360.0, max = 60.0 / 360.0) // bestest
+        val FAINAS_V1 = HueRange(min = 355.0 / 360.0, max = 60.0 / 360.0) // bestest
+        val FAINAS_V2 = HueRange(min = 360.0 / 360.0, max = 35.0 / 360.0) // bestest
 
-        val FAINAS_V1 = HueRange(min = 169.0 / 360.0, max = 69.0 / 360.0) //Emilijos cool
+//        val FAINAS_V1 = HueRange(min = 169.0 / 360.0, max = 69.0 / 360.0) //Emilijos cool
         //val FAINAS_V1 = HueRange(min = 25.0 / 360.0, max = 10.0 / 360.0)
 //        val FAINAS_V1 = HueRange(min = 60.0 / 360.0, max = 180.0 / 360.0)
 //        val FAINAS_V1 = HueRange(min = 355.0 / 360.0, max = 60.0 / 360.0)
@@ -36,13 +37,13 @@ data class ShaderParameters(
     val spatialScale: Double = 0.05,
     val timeScale: Double = 0.124,
     val noiseType: NoiseType = NoiseType.PERLIN,
-    val alphaPower: Double = 4.0,
+    val alphaPower: Double = 2.0,
     val alphaMin: Double = 0.0,
     val alphaMax: Double = 1.0,
     val fbmOctaves: Int = 8,
     val fbmLacunarity: Double = 1.415,
     val fbmGain: Double = 0.593,
-    val hueRange: HueRange = HueRange.FAINAS_V1
+    val hueRange: HueRange = HueRange.FAINAS_V2
 )
 
 enum class NoiseType {
